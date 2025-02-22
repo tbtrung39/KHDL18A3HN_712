a622b1d6afa3e4c4f24c4e9dcd9f69cebd94e9b1
Trang284 <nthtrang.24174600171@sv.uneti.edu.vn>
2025-02-22 17:40:52 +0700

    hoàn thành lab_01

---
+++ lab_01/bai_8.py
@@ -0,0 +1,9 @@
+xA, yA = map(float, input("Nhập tọa độ A (xA yA): ").split()) 
+xB, yB = map(float, input("Nhập tọa độ B (xB yB): ").split()) 
+xC, yC = map(float, input("Nhập tọa độ C (xC yC): ").split()) 
+#  gọi G là trọng tâm của tam giác ABC đã cho  
+xG = (xA + xB + xC) / 3 
+yG = (yA + yB + yC) / 3 
+xG = round(xG, 2) 
+yG = round(yG, 2) 
+print("Tọa độ trọng tâm của tam giác là:", (xG, yG))
