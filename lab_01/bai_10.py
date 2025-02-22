9644c61b2b09b1da944c1822ff37b9ac015c00ed
Trang284 <nthtrang.24174600171@sv.uneti.edu.vn>
2025-02-22 17:36:04 +0700

    hoàn thành lap_01

---
+++ lab_01/bai_10.py
@@ -0,0 +1,5 @@
+import math
+x = float(input("Nhập giá trị x: "))
+fx = (math.log(x) / math.log(4)) + (math.log(2) / math.log(x))
+fx = round(fx, 2) 
+print("Giá trị của biểu thức đã cho là : ", fx) 
