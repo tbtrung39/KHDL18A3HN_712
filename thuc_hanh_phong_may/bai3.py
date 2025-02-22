8f4489e1128f015fe09f532fe5b2871cc2e737ea
Nguyễn Phương Thảo <npthao.24174600146@sv.uneti.edu.vn>
2025-02-22 15:23:44 +0700

    Update bai3.py

---
+++ thuc_hanh_phong_may/bai3.py
@@ -0,0 +1,4 @@
+x1, y1, z1 = map(float,input("nhập tọa độ vecto a: ").split())
+x2, y2, z2 = map(float,input("nhập tọa độ vecto b: ").split())
+tvh=x1*x2+y1*y2+z1*z2
+print(f"tích vô hướng của 2 vecto a và b là: {tvh:.2f}")
