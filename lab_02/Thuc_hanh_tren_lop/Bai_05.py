2ea34c7af9ae65cc617c18406693af7b189751bd
DaoTienThinhDHKL18A3 <dtthinh.24174600154@sv.uneti.edu.vn>
2025-02-22 22:27:22 +0700

    Bai_05.py

---
+++ lab_02/Thuc_hanh_tren_lop/Bai_05.py
@@ -0,0 +1,28 @@
+# bài 5
+t = int(input("Nhập tháng (1-12): "))
+if t == 1:
+    print("Tháng một")
+elif t == 2:
+    print("Tháng hai")
+elif t == 3:
+    print("Tháng ba")
+elif t == 4:
+    print("Tháng bốn")
+elif t == 5:
+    print("Tháng năm")
+elif t == 6:
+    print("Tháng sáu")
+elif t == 7:
+    print("Tháng bảy")
+elif t == 8:
+    print("Tháng tám")
+elif t == 9:
+    print("Tháng chín")
+elif t == 10:
+    print("Tháng mười")
+elif t == 11:
+    print("Tháng mười một")
+elif t == 12:
+    print("Tháng mười hai")
+else:
+    print("Nhập sai vui lòng thử lại")
