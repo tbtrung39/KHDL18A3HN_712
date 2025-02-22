e8c247843e9f95d5c11566c2eff75082a5f1e8e2
Nguyễn Phương Thảo <npthao.24174600146@sv.uneti.edu.vn>
2025-02-22 15:25:58 +0700

    Update bai7.py

---
+++ thuc_hanh_phong_may/bai7.py
@@ -0,0 +1,6 @@
+xa,ya=map(float,input("nhap toa do dinh A cua tam giac: ").sp
+xb,yb=map(float,input("nhap toa do dinh B cua tam giac: ").sp
+xc,yc=map(float,input("nhap toa do dinh C cua tam giac: ").sp
+xg=(xa+xb+xc)/3
+yg=(ya+yb+yc)/3
+print(f"trong tam cua tam giac ABC la: G({xg:.2f},{yg:.2f})")
