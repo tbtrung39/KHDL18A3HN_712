eef6f9e496da71af2a33f8bf05f2cecf9739b543
DaoTienThinhDHKL18A3 <dtthinh.24174600154@sv.uneti.edu.vn>
2025-02-22 22:26:42 +0700

    Update Bai_03.py

---
+++ lab_02/Thuc_hanh_tren_lop/Bai_03.py
@@ -0,0 +1,18 @@
+# Bài3 
+thu = float(input("Nhập thứ (1-7): "))
+if thu == 1:
+    print("Chủ nhậtt")
+elif thu == 2:
+    print("")
+elif thu == 3:
+    print("Thứ ba")
+elif thu == 4:
+    print("Thứ tư")
+elif thu == 5:
+    print("Thứ năm")
+elif thu == 6:
+    print("Thứ sáu")
+elif thu == 7:
+    print("Thứ bảy")
+else:
+    print("Nhập sai vui lòng thử lại")
