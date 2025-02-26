98b3c2087fc452c82749b32da9ab36fb61d82b91
Hoangmai1562005 <htmai.24174600153@sv.uneti.edu.vn>
2025-02-26 16:53:07 +0700

    Update bai1.py

---
+++ thuc_hanh_lab01/bai1.py
@@ -0,0 +1,14 @@
+# Nhập thông tin sinh viên
+ma_so = input("Nhập mã số sinh viên: ")
+ho_ten = input("Nhập họ tên: ")
+que_quan = input("Nhập quê quán: ")
+nam_sinh = int(input("Nhập năm sinh: "))
+diem_trung_binh = float(input("Nhập điểm trung bình: "))
+
+# Xuất thông tin sinh viên
+print("\n--- Thông tin sinh viên ---")
+print("Mã số:", ma_so)
+print("Họ tên:", ho_ten)
+print("Quê quán:", que_quan)
+print("Năm sinh:", nam_sinh)
+print("Điểm trung bình:", diem_trung_binh)
