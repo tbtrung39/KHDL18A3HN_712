6576d3686dcb754f0347c0bd1d1860fd7dea404c
Quang <quang180226@gmail.com>
2025-02-28 11:51:31 +0700

    cập nhật

--- lab03/bai11a.py
+++ lab03/bai11a.py
@@ -3,12 +3,15 @@ chieu_rong = 2 * (so_n + 1) - 1
 for i in range(so_n):
     if i == 0:
         hang = '*'
+        do_dai_hang = 1
     elif i == so_n - 1:
         hang = '* ' * (so_n + 1)
+        do_dai_hang = 2 * (so_n + 1) - 1  
     else:
         khoang_trong = 2 * i - 1
         hang = '*' + ' ' * khoang_trong + '*'
-    khoang_trang_ben_trai = (chieu_rong - len(hang)) // 2
-    print(" " * khoang_trang_ben_trai + hang)
+        do_dai_hang = 2 + khoang_trong 
+    khoang_trang_ben_trai = (chieu_rong - do_dai_hang) // 2
+    print(" " * khoang_trang_ben_trai + hang)   
     if i != so_n - 1:
         print()
