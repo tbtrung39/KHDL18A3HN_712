00774481640d7a683bea1dfef7051dcb36e57727
Thu Trang <pttrang.24174600152@sv.uneti.edu.vn>
2025-03-03 22:11:49 +0700

    Hoàn thành bài 5/Lab01

---
+++ thuc_hanh_Lab01/bài_5.py
@@ -0,0 +1,12 @@
+#tính tích vô hướng
+n = int(input("Nhập số phần tử của vector: "))
+a1 = float(input("Nhập a1: "))
+a2 = float(input("Nhập a2: "))
+a3 = float(input("Nhập a3: "))
+a4 = float(input("Nhập a4: "))
+b1 = float(input("Nhập b1: "))
+b2 = float(input("Nhập b2: "))
+b3 = float(input("Nhập b3: "))
+b4 = float(input("Nhập b4: "))
+tich_vo_huong = a1 * b1 + a2 * b2 + a3 * b3 + a4 * b4
+print(f"Tích vô hướng của hai vector là: {tich_vo_huong:.2f}")
