01766c9b530c046d4f1d9bd13085434f0744a4e3
Quang <quang180226@gmail.com>
2025-03-10 09:44:34 +0700

    a

--- lab03/bai11b.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Nhập số n: "))
-for i in range(1, n):
-    print(' ' * (n - i), end='')  
-    if i > 1:
-        print('*' + ' ' * (2 * (i - 1) - 1) + '*')  
-    else:
-        print('*')  
-print('* ' * n)
--- lab03/bai5.py
+++
@@ -1,6 +0,0 @@
-chieu_dai = int(input("Nhập chiều dài của hình chữ nhật: "))
-chieu_rong = int(input("Nhập chiều rộng của hình chữ nhật: "))
-for i in range(chieu_rong):
-    for j in range(chieu_dai):
-        print('*', end=' ')
-    print() 
