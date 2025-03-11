76be6353e0b224709f0a0403120cf5664332190d
tly-n <ntlinh.24174600155@sv.uneti.edu.vn>
2025-03-11 22:30:46 +0700

    da hoan thanh lab 04

---
+++ lab_04/bai10.py
@@ -0,0 +1,31 @@
+so=input("Nhập một số bất kì: ")
+chu=''
+i=0
+while i<len(so):
+    chu_so=so[i]
+    if chu_so=='0':
+        chu+='không '
+    elif chu_so=='1':
+        chu+='một '
+    elif chu_so=='2':
+        chu+='hai '
+    elif chu_so=='3':
+        chu+='ba '
+    elif chu_so=='4':
+        chu+='bốn '
+    elif chu_so=='5':
+        chu+='năm '
+    elif chu_so=='6':
+        chu+='sáu '
+    elif chu_so=='7':
+        chu+='bảy '
+    elif chu_so=='8':
+        chu+='tám '
+    elif chu_so=='9':
+        chu+='chín '
+    elif chu_so=='-':
+        chu+='âm '
+    elif chu_so==".":
+        chu+='phẩy '
+    i+=1
+print(chu)
---
+++ lab_04/bai8.py
@@ -0,0 +1,2 @@
+n=input('nhap ki tu: ')
+print('gia tri ASCII cua ki tu:', ord(n))
