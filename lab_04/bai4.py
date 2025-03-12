6f4e3d4509e635b868c528bd7ed827e7c5b507a3
Nguyễn Phương Thảo <npthao.24174600146@sv.uneti.edu.vn>
2025-03-12 13:49:14 +0700

    đã thực hiện xong lab 04

---
+++ lab_04/bai4.py
@@ -0,0 +1,6 @@
+tu_so = int(input("Nhập tử số: "))
+mau_so = int(input("Hãy nhâp mẫu số : "))
+while mau_so == 0:
+    print("Mẫu số không hợp lệ. Vui lòng nhập lại!")
+    mau_so = int(input("Nhập mẫu số: "))
+print("Phân số vừa nhập là:", tu_so, "/", mau_so)
---
+++ lab_04/bai9.py
@@ -0,0 +1,32 @@
+so = input("hay nahp so: ")
+tong = 0
+hop_le = True
+for chu_so in so:
+    if chu_so == "0":
+        tong += 0
+    elif chu_so == "1":
+        tong += 1
+    elif chu_so == "2":
+        tong += 2
+    elif chu_so == "3":
+        tong += 3
+    elif chu_so == "4":
+        tong += 4
+    elif chu_so == "5":
+        tong += 5
+    elif chu_so == "6":
+        tong += 6
+    elif chu_so == "7":
+        tong += 7
+    elif chu_so == "8":
+        tong += 8
+    elif chu_so == "9":
+        tong += 9
+    elif chu_so == "-":
+        continue  
+    else:
+        print("Số nhập không hợp lệ")
+        hop_le = False
+        break
+if hop_le:
+    print("Tổng các chữ số là:", tong)
