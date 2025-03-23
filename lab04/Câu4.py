58a05f7472e221db629f01c76d5d1ba2846ae28c
Quang <quang180226@gmail.com>
2025-03-23 08:54:18 +0700

    a

--- lab04/Câu4.py
+++
@@ -1,8 +0,0 @@
-tu_so=int(input('Nhập tử số của phân số: '))
-while True:
-    mau_so=int(input('Nhập mẫu số của phân số: '))
-    if mau_so==0:
-        print("Vui lòng nhập mẫu số khác 0")
-    else:
-        break
-print(f"Phân số vừa nhập là: {tu_so}/{mau_so}")
--- lab04/Câu5.py
+++
@@ -1,5 +0,0 @@
-while True:
-    so=float(input("Nhập một số bất kì: "))
-    if so<0:
-        print("Bạn đã nhập số âm, chương trình sẽ dừng lại.")
-        break
