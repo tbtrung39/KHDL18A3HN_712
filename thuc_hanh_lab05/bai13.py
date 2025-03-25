f3bfdde3f868f2b18bc48f3874c00067f8b0334c
quyenpuns1 <nthquyen.24174600145@sv.uneti.edu.vn>
2025-03-25 22:42:42 +0700

    hoàn thành lab05

---
+++ thuc_hanh_lab05/bai13.py
@@ -0,0 +1,7 @@
+S1=input("Nhập chuỗi ký tự S1:")
+S2=input("Nhập chuỗi ký tự S2:")
+vtc=S1.rfind(S2)
+
+
+
+print("Vị trí xuất hiện sau cùng của chuỗi/xâu kí tự '",S2,"'trong\chuỗi'",S1,"là: %d"%vtc)
---
+++ thuc_hanh_lab05/bai7.py
@@ -0,0 +1,18 @@
+chuoi = input("Nhập chuỗi ký tự: ")
+chuoi_so = "" 
+for ky_tu in chuoi:
+    if ky_tu.isdigit():
+        chuoi_so += ky_tu  
+if chuoi_so:
+    so_nguyen = int(chuoi_so)  
+    print("Chuỗi số sau khi lọc:", so_nguyen) 
+    tong_uoc = 0
+    for i in range(1, so_nguyen):  
+        if so_nguyen % i == 0:
+            tong_uoc += i  
+    if tong_uoc == so_nguyen:
+        print(so_nguyen, "là số hoàn hảo.")
+    else:
+        print(so_nguyen, "không phải là số hoàn hảo.")
+else:
+    print("Chuỗi không chứa số nào.")
