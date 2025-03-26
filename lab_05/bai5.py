468409e384b03d7854912f00fdb15baa3f3d22c1
Daothiquyen24174600129 <Daothiquyen021226@gmail.com>
2025-03-26 11:00:31 +0700

    bai5.py

--- lab_05/bai5.py
+++ lab_05/bai5.py
@@ -1,20 +1,18 @@
-# cau 5
 c_ky_tu = input("Nhập chuỗi ký tự: ")
 chuoi_so = ""
 for ky_tu in c_ky_tu:
-    if ky_tu.isdigit():  
+    if ky_tu.isdigit():
         chuoi_so += ky_tu  
 if chuoi_so == "":
     so_nguyen = 0
 else:
-    so_nguyen = int(chuoi_so)  
-def la_so_hoan_hao(n):
-    if n <= 0:
-        return False
-    tong_uoc = sum(i for i in range(1, n) if n % i == 0)
-    return tong_uoc == n
+    so_nguyen = int(chuoi_so)
+tong_uoc = 0
+for i in range(1, so_nguyen):
+    if so_nguyen % i == 0:
+        tong_uoc += i  
 print("Chuỗi số sau khi lọc:", chuoi_so)
-if la_so_hoan_hao(so_nguyen):
+if tong_uoc == so_nguyen and so_nguyen > 0:
     print(so_nguyen, "là số hoàn hảo.")
 else:
     print(so_nguyen, "không phải là số hoàn hảo.")
