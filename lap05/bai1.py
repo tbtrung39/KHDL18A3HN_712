a77c78241f13fe81c50ce60752e1c1a322c11bb2
unknown <tttan.24174600140@sv.uneti.edu.vn>
2025-03-26 21:22:49 +0900

    ghi những gì mình đã thực hiện

---
+++ lap05/bai1.py
@@ -0,0 +1,16 @@
+#Cách 1
+ s = input("Nhập chuỗi kí tự Str là: ")
+ dem = 0
+ for c in (s):
+     if c.isdigit():
+         dem += 1
+ print("số kí tự là số trong chuỗi là: ",dem)
+ #Cách 2
+ s = input("Nhập chuỗi kí tự Str là: ")
+ dem = 0
+ for k in (s):
+     if "0" <= k <= "9":
+         dem += 1
+     else:
+         0
+ print("số kí tự là số trong chuỗi là: ",dem)
