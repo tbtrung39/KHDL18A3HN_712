906f4d086de8df1248e4cc5163e98d5e0a4c822a
Dinh Huu Viet <dhviet.24174600149@sv.uneti.edu.vn>
2025-04-15 00:07:54 +0700

    hoàn thanh lab02

---
+++ lab02/cau4.py
@@ -0,0 +1,6 @@
+so_nguyen = int(input("Nhập vào một số nguyên: "))
+if so_nguyen >= 100:
+    chu_so_hang_tram = (so_nguyen // 100) % 10
+    print("Chữ số hàng trăm là:", chu_so_hang_tram)
+else:
+    print("0")
