16fdcd98e843f9c639fd767ef764d6e09174e1ce
tly-n <ntlinh.24174600155@sv.uneti.edu.vn>
2025-05-06 22:53:07 +0700

    da hoan thanh lab 09

---
+++ lab_09/cau7.py
@@ -0,0 +1,10 @@
+def bo_nghiem(N, n, x=[], tong=0):
+    if len(x) == n:
+        if tong == N:
+            print(x)
+        return
+    for i in range(N - tong + 1):
+        bo_nghiem(N, n, x + [i], tong + i)
+N = int(input("Nhap so tu nhien N: "))
+n = int(input("Nhap so tu nhien n: "))
+bo_nghiem(N, n)
