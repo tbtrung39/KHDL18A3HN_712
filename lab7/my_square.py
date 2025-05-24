dad03272df3ef05d5a0dbaca44b681e7fff023cb
minhthang <thangday1512@gmail.com>
2025-05-24 13:51:43 +0700

    đã thực hiện lab10

---
+++ lab7/my_square.py
@@ -0,0 +1,5 @@
+# bai 2
+def ChuviHinhVuong(a):
+    return 4 * a
+def DientichHinhVuong(a):
+    return a * a
---
+++ lab7/sohoc.py
@@ -0,0 +1,12 @@
+# bai 3
+import math
+def Ucln(a, b):
+    return math.gcd(a, b)
+def Bcnn(a, b):
+    return abs(a * b) // math.gcd(a, b)
+def SumDivisor(n):
+    tong = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            tong += i
+    return tong
