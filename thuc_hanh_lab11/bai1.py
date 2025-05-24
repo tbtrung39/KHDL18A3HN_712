116286db6586d32171268375de5c1cb6078654f8
Hoangmai1562005 <htmai.24174600153@sv.uneti.edu.vn>
2025-05-25 01:11:25 +0700

    hoan thanh 1,2,3,4,5,6,7,8,9,10

---
+++ thuc_hanh_lab11/bai1.py
@@ -0,0 +1,5 @@
+with open('dayso.dat', 'r') as tep:
+    du_lieu = tep.read()  
+cac_so = map(int, du_lieu.split())
+tong_le = sum(so for so in cac_so if so % 2 != 0)
+print("tong cac so le trong day la", tong_le)
