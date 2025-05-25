e8688f9d837191f5d19cdf076d0d5cb5211c3937
unknown <tttan.24174600140@sv.uneti.edu.vn>
2025-05-25 16:58:21 +0900

    hoan thanh lap09

---
+++ Lap09/bai11.py
@@ -0,0 +1,15 @@
+def giai_thua_kep(n):
+    if n == 0 or n == 1:
+        return 1
+    return giai_thua_kep(n - 2) * n
+
+def tinh_tong(k):
+    tong = 0
+    for i in range(1, k + 1):
+        if i % 2 == 1:
+            tong += giai_thua_kep(i)
+        else:  
+            tong -= giai_thua_kep(i)
+    return tong
+k = int(input("hay nhap gia tri k nho hon 1000: "))
+print("Tong S = ", tinh_tong(k))
---
+++ Lap09/bai8.py
@@ -0,0 +1,33 @@
+def cau_a(n):
+    if n == 1:
+        return 1 / (1 * 2)
+    return 1 / (n * (n + 1)) + cau_a(n - 1)
+n = int(input("Nhap n: "))
+print("S = ", cau_a(n))
+def cau_b(n):
+    if n == 1:
+        return 1
+    return 1 / tinh_giai_thua(n) + cau_b(n - 1)
+def tinh_giai_thua(x):
+    if x == 1:
+        return 1
+    return x * tinh_giai_thua(x - 1)
+n = int(input("Nhap n tu ban phim: "))
+print("S = ", cau_b(n))
+import math
+def cau_c(n):
+    if n == 1:
+        return math.sqrt(3)
+    return math.sqrt(3 * n + cau_c(n - 1))
+n = int(input("Nhap n tu ban phim: "))
+print("S = ", cau_c(n))
+import math
+def cau_d(n):
+    if n == 1:
+        return math.sqrt(1) ** (1 / (n + 1)) 
+    return (n + cau_d(n - 1)) ** (1 / n)
+
+n = int(input("Nhap n tu ban phim: "))
+result = cau_d(n)
+result = result ** (1 / (n + 1))
+print("S = ", result)
